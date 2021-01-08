fa8f7ade4764a2af66dd17d81b885075995fad2b
yanzzbot <77144968+yanzzbot@users.noreply.github.com>
2021-01-09 03:38:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 * ARIS187 ID
 * BOLEH UBAH TAPI KECUALI INFO!!!
 */
-const A187 = '👾BANGLENT👾'; // JANGAN UBAH-UBAH INFO!!!
-const instagram = 'https://instagram.com/_maslent11'; // JANGAN UBAH-UBAH INFO!!!
-const nomer = 'Wa.me/+628165466368'; // JANGAN UBAH-UBAH INFO!!!
+const A187 = '👾YANZZBOT👾'; // JANGAN UBAH-UBAH INFO!!!
+const instagram = 'https://instagram.com/_zxuan54'; // JANGAN UBAH-UBAH INFO!!!
+const nomer = 'Wa.me/+6281803522862'; // JANGAN UBAH-UBAH INFO!!!
 const aktif = 'Tergantung jaringan'; // JANGAN UBAH-UBAH INFO!!!
 const groupwhatsapp = 'https://chat.whatsapp.com/E8VriS0ojj53PX5wkhPYaD'; // JANGAN UBAH-UBAH INFO!!!
-const youtube = 'https://youtu.be/pRTmMIfkfT0'; // JANGAN UBAH-UBAH INFO!!!
+const youtube = 'https://youtu.be/YANZZ OFFICIAL'; // JANGAN UBAH-UBAH INFO!!!
 //A187ID
 const qrcode = require("qrcode-terminal");
 const moment = require("moment");
